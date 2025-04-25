d4a44ce190f80ae634ca6d6573b7297827e705bf
gunturor <mail@guntur.eu.org>
2025-04-25 22:16:39 +0700

    Update about.js

--- about.js
+++ about.js
@@ -1,28 +1,30 @@
-const texts = ["Front-End Developer", "UI/UX Enthusiast", "Tech Enthusiast"];
-let count = 0;
-let index = 0;
-let currentText = "";
-let letter = "";
-(function type() {
-  if (count === texts.length) count = 0;
-  currentText = texts[count];
-  letter = currentText.slice(0, ++index);
-  document.getElementById("typing").textContent = letter;
-  if (letter.length === currentText.length) {
-    count++;
-    index = 0;
-    setTimeout(type, 2000);
-  } else {
-    setTimeout(type, 100);
+window.addEventListener("load", () => {
+  const texts = ["Front-End Developer", "UI/UX Enthusiast", "Tech Enthusiast"];
+  let count = 0;
+  let index = 0;
+  let currentText = "";
+  let letter = "";
+  (function type() {
+    if (count === texts.length) count = 0;
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    document.getElementById("typing").textContent = letter;
+    if (letter.length === currentText.length) {
+      count++;
+      index = 0;
+      setTimeout(type, 2000);
+    } else {
+      setTimeout(type, 100);
+    }
+  })();
+  const audio = document.getElementById("audio");
+  const albumArt = document.getElementById("albumArt");
+  if (audio && albumArt) {
+    audio.addEventListener("play", () => {
+      albumArt.classList.add("rotate");
+    });
+    audio.addEventListener("pause", () => {
+      albumArt.classList.remove("rotate");
+    });
   }
-})();
-const audio = document.getElementById("audio");
-const albumArt = document.getElementById("albumArt");
-if (audio && albumArt) {
-  audio.addEventListener("play", () => {
-    albumArt.classList.add("rotate");
-  });
-  audio.addEventListener("pause", () => {
-    albumArt.classList.remove("rotate");
-  });
-}
+});
